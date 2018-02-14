cf423fe3387c1e7c4bcc009701dbfb39d567fb14
subrat82 <subratamohapatra82@gmail.com>
2018-02-13 20:31:31 -0700

    First commit

--- java-maven-app/src/main/java/com/mycompany/app/App.java
+++ java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World123!";
+    private final String message = "Hello World12888888888888883!";
 
     public App() {}
 
