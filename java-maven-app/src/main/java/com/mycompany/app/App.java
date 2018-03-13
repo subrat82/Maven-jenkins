2b247f6fce3ed0f6203062a63f5a9d7156007dfe
subrat82 <subratamohapatra82@gmail.com>
2018-03-12 20:45:44 -0700

    file checkin

--- java-maven-app/src/main/java/com/mycompany/app/App.java
+++ java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World666!";
+    private final String message = "Hello World666hhhhhh!";
 
     public App() {}
 
