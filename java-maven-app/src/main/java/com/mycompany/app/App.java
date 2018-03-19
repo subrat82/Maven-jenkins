fa047ee3883002231b1a1977a63ca3ab9b5fc33b
subrat82 <subratamohapatra82@gmail.com>
2018-03-19 11:06:10 -0700

    modified

--- java-maven-app/src/main/java/com/mycompany/app/App.java
+++ java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Mr Subrat & Sasmita";
+    private final String message = "Hello World! Mr Subrat Subrat";
 
     public App() {}
 
